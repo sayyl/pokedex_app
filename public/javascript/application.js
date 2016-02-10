0500ff8e2ab557705cdd9fab229eacc2819bde90
distal-shores <samcoll@gmail.com>
2016-02-10 01:56:41 +0000

    fixed show issues

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,13 +1,6 @@
 $(function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> tyne
-=======
 
 
->>>>>>> samsbranch
   $("#showPokemon").on('click', function() {
     $("#pokemonForm").hide();
     $.getJSON('/pokemons', function(pokemons) {
@@ -21,47 +14,34 @@ $(function() {
       }); 
     });
   }); 
-<<<<<<< HEAD
 
 
-  $("#addPokemon").on('click', function() {
-    $("#results").hide();
-    $("#pokemonForm").show();
-  });
+    $("#addPokemon").on('click', function() {
+      $("#results").hide();
+      $("#pokemonForm").show();
+    });
 
-   $("#newPokemon").on('submit', function() {
-    var name = $("#name").val();  
-    var element = $("#element").val();
-    var level = $("#level").val();
+     $("#newPokemon").on('submit', function() {
+      var name = $("#name").val();  
+      var element = $("#element").val();
+      var level = $("#level").val();
 
-    if (name == "" || element == "" || level == "") {
-      alert("You must fill out all fields, fool.");
-      return false;
-    }
-
-     $.post('/pokemons', {name: name, element: element, level: level}, function(data) {
-      if (data.result) {
-        $("#name").add("#element").add("#level").val('');
-        alert("Yay it worked. ID is: " + data.id);
-      } else {
-        alert("OOPS");
+      if (name == "" || element == "" || level == "") {
+        alert("You must fill out all fields, fool.");
+        return false;
       }
-    }, 'json');
 
-    return false;
+       $.post('/pokemons', {name: name, element: element, level: level}, function(data) {
+        if (data.result) {
+          $("#name").add("#element").add("#level").val('');
+          alert("Yay it worked. ID is: " + data.id);
+        } else {
+          alert("OOPS");
+        }
+      }, 'json');
+
+      return false;
+    });
   });
-});
 
 
-
- 
-
-
-  
-
-<<<<<<< HEAD
-=======
->>>>>>> tyne
-}); 
-=======
->>>>>>> samsbranch
