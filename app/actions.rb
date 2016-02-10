6ac0be74a528beaf7af4956ab4bbef2298211660
distal-shores <samcoll@gmail.com>
2016-02-10 01:50:01 +0000

    added the add

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,17 @@ end
 get '/pokemons' do
   Pokemon.all.to_json
 end
+
+post '/pokemons' do
+  name = params[:name]
+  element = params[:element]
+  level = params[:level]
+  results = {result: false}
+
+  pokemon = Pokemon.new(name: name, element: element, level: level.to_i)
+  if pokemon.save
+    results[:result] = true
+    results[:id] = pokemon.id
+  end
+  results.to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,9 +1,11 @@
 $(function() {
+
+
   $("#showPokemon").on('click', function() {
     $("#pokemonForm").hide();
     $.getJSON('/pokemons', function(pokemons) {
       var table = $("#pokemons").find('tbody').empty();
-      players.forEach(function(pokemon) {  
+      pokemons.forEach(function(pokemon) {  
         var tr = $("<tr>").addClass('pokemon').appendTo(table);
         $("<td>").appendTo(tr).text(pokemon.name);
         $("<td>").appendTo(tr).text(pokemon.element);
@@ -13,5 +15,38 @@ $(function() {
     });
   }); 
 
+  $("#addPokemon").on('click', function() {
+    $("#results").hide();
+    $("#pokemonForm").show();
+  });
 
-}); 
+   $("#newPokemon").on('submit', function() {
+    var name = $("#name").val();  
+    var element = $("#element").val();
+    var level = $("#level").val();
+
+    if (name == "" || element == "" || level == "") {
+      alert("You must fill out all fields, fool.");
+      return false;
+    }
+
+     $.post('/pokemons', {name: name, element: element, level: level}, function(data) {
+      if (data.result) {
+        $("#name").add("#element").add("#level").val('');
+        alert("Yay it worked. ID is: " + data.id);
+      } else {
+        alert("OOPS");
+      }
+    }, 'json');
+
+    return false;
+  });
+});
+
+
+
+ 
+
+
+  
+
